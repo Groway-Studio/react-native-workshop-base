36ba4dc7671ebcecb2f649984efe2cafc651624f
misa <misael.riojas@fulltimeforce.com>
2022-03-31 09:18:22 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,93 +1,50 @@
 import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  configureFonts,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-function Section({children = null, title = ''}) {
-  var isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text> {title} </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {color: isDarkMode ? Colors.light : Colors.dark},
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const fontConfig = {};
+const customPaperTheme = (dark: boolean): ReactNativePaper.Theme => ({
+  ...DefaultTheme,
+  dark,
+  fonts: configureFonts(fontConfig),
+  colors: {
+    ...DefaultTheme.colors,
+  },
+});
 
 const App = () => {
-  var isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const dark = true;
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <PaperProvider theme={customPaperTheme(dark)}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.red} />
+        <View style={styles.orange} />
+        <View style={styles.green} />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  red: {
+    backgroundColor: 'red',
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  orange: {
+    backgroundColor: 'darkorange',
+    flex: 5,
   },
-  highlight: {
-    fontWeight: '700',
+  green: {
+    backgroundColor: 'green',
+    flex: 3,
   },
 });
 
