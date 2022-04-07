79f3e0cf6b15e8d73fb8a8074337c81425dd76ba
misa <misael.riojas@fulltimeforce.com>
2022-04-07 16:08:29 -0500

    init ios

--- App.tsx
+++ App.tsx
@@ -1,3 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
 import React from 'react';
 import {
   SafeAreaView,
@@ -17,25 +27,36 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-function Section({children = null, title = ''}) {
-  var isDarkMode = useColorScheme() === 'dark';
-
+const Section: React.FC<{
+  title: string;
+}> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text> {title} </Text>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
       <Text
         style={[
           styles.sectionDescription,
-          {color: isDarkMode ? Colors.light : Colors.dark},
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
         ]}>
         {children}
       </Text>
     </View>
   );
-}
+};
 
 const App = () => {
-  var isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
